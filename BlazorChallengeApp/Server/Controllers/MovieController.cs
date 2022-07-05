cc47334e6fe19afcdef405163b6991b9715c45be
Elisa Niyonkuru <elisa.niyonkuru@skillsbox.com>
2022-07-05 12:00:55 +0200

    Added  View Movie Details method.

--- BlazorChallengeApp/Server/Controllers/MovieController.cs
+++ BlazorChallengeApp/Server/Controllers/MovieController.cs
@@ -57,87 +57,20 @@ namespace BlazorChallengeApp.Server.Controllers
                 return NotFound("Can't save Movies...");
             }
         }
-        
-        /*        // GET: MovieController/Details/5
-                public ActionResult Details(int id)
-                {
-                    return View();
-                }
+        // GET: MovieController/Details/5
+        [HttpGet("Details")]
+        public ActionResult Details(int id)
+        {
+            try
+            {
+                var movie = _dbContext.Movie.FirstOrDefault(x => x.Id == id);
+                return Ok(movie);   
+            }catch(Exception e)
+            {
 
-                // GET: MovieController/Create
-                public ActionResult Create()
-                {
-                    return View();
-                }
+                return NotFound("Movie Not Found.");
+            }
 
-                // POST: MovieController/Create
-                [HttpPost]
-                [ValidateAntiForgeryToken]
-                public ActionResult Create(IFormCollection collection)
-                {
-                    try
-                    {
-                        return RedirectToAction(nameof(Index));
-                    }
-                    catch
-                    {
-                        return View();
-                    }
-                }
-
-                        [HttpPost]
-                [ValidateAntiForgeryToken]
-                public ActionResult Create(IFormCollection collection)
-                {
-                    try
-                    {
-                        return RedirectToAction(nameof(Index));
-                    }
-                    catch
-                    {
-                        return View();
-                    }
-                }
-                // GET: MovieController/Edit/5
-                public ActionResult Edit(int id)
-                {
-                    return View();
-                }
-
-                // POST: MovieController/Edit/5
-                [HttpPost]
-                [ValidateAntiForgeryToken]
-                public ActionResult Edit(int id, IFormCollection collection)
-                {
-                    try
-                    {
-                        return RedirectToAction(nameof(Index));
-                    }
-                    catch
-                    {
-                        return View();
-                    }
-                }
-
-                // GET: MovieController/Delete/5
-                public ActionResult Delete(int id)
-                {
-                    return View();
-                }
-
-                // POST: MovieController/Delete/5
-                [HttpPost]
-                [ValidateAntiForgeryToken]
-                public ActionResult Delete(int id, IFormCollection collection)
-                {
-                    try
-                    {
-                        return RedirectToAction(nameof(Index));
-                    }
-                    catch
-                    {
-                        return View();
-                    }
-                }*/
+        }
     }
 }
