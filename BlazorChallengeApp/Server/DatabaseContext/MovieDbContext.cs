1eae123c4d3083fd3dc13a7e65256513b3865345
Elisa Niyonkuru <elisa.niyonkuru@skillsbox.com>
2022-07-05 17:51:31 +0200

    Created Tickets table and it's migration.

--- BlazorChallengeApp/Server/DatabaseContext/MovieDbContext.cs
+++ BlazorChallengeApp/Server/DatabaseContext/MovieDbContext.cs
@@ -19,6 +19,7 @@ namespace BlazorChallengeApp.Server.DatabaseContext
         #region Public properties
         public DbSet<Movie> Movie { get; set; }
         public DbSet<RunningTimes> RunningTimes { get; set; }
+        public DbSet<Ticket> Ticket { get; set; }
         #endregion
 
         #region Overidden methods
@@ -28,6 +29,11 @@ namespace BlazorChallengeApp.Server.DatabaseContext
                 (c1, c2) => c1.SequenceEqual(c2),
                 c => c.Aggregate(0, (a, v) => HashCode.Combine(a, v.GetHashCode())),
                 c => c.ToList());
+
+            // Populate Tickets on creationg of the database
+
+            modelBuilder.Entity<Ticket>().HasData(GetTickets());
+
             modelBuilder.Entity<RunningTimes>().Property(p => p.Mon)
       .HasConversion(v => JsonConvert.SerializeObject(v),
                      v => JsonConvert.DeserializeObject<List<string>>(v)).Metadata.SetValueComparer(valueComparer);
@@ -49,7 +55,23 @@ namespace BlazorChallengeApp.Server.DatabaseContext
             modelBuilder.Entity<RunningTimes>().Property(p => p.Sun)
       .HasConversion(v => JsonConvert.SerializeObject(v),
                      v => JsonConvert.DeserializeObject<List<string>>(v)).Metadata.SetValueComparer(valueComparer);
+
             base.OnModelCreating(modelBuilder);
+
+        }
+        #endregion
+
+        #region Private methods
+        private List<Ticket> GetTickets()
+        {
+            return new List<Ticket>
+            {
+                new Ticket { Id = Guid.NewGuid().ToString(), _ticketName = "ADULT", _price= 10.00 },
+                new Ticket { Id = Guid.NewGuid().ToString(), _ticketName = "CHILD (AGE 14 AND UNDER)", _price= 10.00 },
+                new Ticket { Id = Guid.NewGuid().ToString(), _ticketName = "FAMILY X 4(2 AD + 2CH OR 1AD +3 CH)", _price= 32.00 },
+                new Ticket { Id = Guid.NewGuid().ToString(), _ticketName = "STUDENT", _price= 8.50 },
+                new Ticket { Id = Guid.NewGuid().ToString(), _ticketName = "SENIOR(65 & OVER)", _price= 8.50 },
+            };
         }
         #endregion
     }
---
+++ BlazorChallengeApp/Shared/Ticket.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorChallengeApp.Shared
+{
+    public class Ticket
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; } = String.Empty;
+        public string _ticketName { get; set; } = String.Empty;
+        public double _price { get; set; } = 0;
+    }
+}
