5385c48faac381c23d5506eda77b0f30a0301db6
Elisa Niyonkuru <elisa.niyonkuru@skillsbox.com>
2022-07-06 09:08:33 +0200

    Changed Method name

---
+++ BlazorChallengeApp/Server/Controllers/TicketController.cs
@@ -0,0 +1,28 @@
+﻿using BlazorChallengeApp.Server.DatabaseContext;
+using BlazorChallengeApp.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorChallengeApp.Server.Controllers
+{
+    public class TicketController
+    {
+        private MovieDbContext _dbContext;
+        public TicketController(MovieDbContext movieDbContext)
+        {
+            _dbContext = movieDbContext;
+        }
+
+        // GET: TicketController
+        [HttpGet("All")]
+        public List<Ticket> Index()
+        {
+            var output = new List<Ticket>();
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+            output = (from T in _dbContext.Ticket
+                      select T).ToList();
+
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+            return output;
+        }
+    }
+}
--- BlazorChallengeApp/Shared/Ticket.cs
+++ BlazorChallengeApp/Shared/Ticket.cs
@@ -9,7 +9,7 @@ namespace BlazorChallengeApp.Shared
 {
     public class Ticket
     {
-        [JsonProperty("id")]
+        
         public string Id { get; set; } = String.Empty;
         public string _ticketName { get; set; } = String.Empty;
         public double _price { get; set; } = 0;
