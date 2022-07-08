da6ddfc673f5a193d44a5c4dc4da009bf3e74832
Elisa Niyonkuru <elisa.niyonkuru@skillsbox.com>
2022-07-08 13:32:33 +0200

    Partial Push, Will go back to it.

---
+++ BlazorChallengeApp/Server/CQRS/Commands/Booking/CreateBooking.cs
@@ -0,0 +1,34 @@
+﻿using BlazorChallengeApp.Server.DatabaseContext;
+using MediatR;
+using System.Linq;
+
+namespace BlazorChallengeApp.Server.CQRS.Commands.Booking
+{
+    public class CreateBooking
+    {
+        // Data to execute
+        public record Command(Shared.Booking Booking) : IRequest<Response>;
+        // Handler
+        public class Handler : IRequestHandler<Command, Response>
+        {
+            MovieDbContext movieDbContext;
+            public Handler(MovieDbContext movieDbContext)
+            {
+                this.movieDbContext = movieDbContext;
+            }
+            public async Task<Response> Handle(Command request, CancellationToken cancellationToken)
+            {
+                Console.WriteLine("Here");
+                // Saving Booking to database;
+                movieDbContext.Add(movieDbContext.Booking.Where(booking => booking.Name != request.Booking.Name && booking.Email != request.Booking.Email));
+                movieDbContext.SaveChanges();
+
+                return new Response(true);
+            }
+        }
+
+        // Response
+
+        public record Response(bool Saved);
+    }
+}
---
+++ BlazorChallengeApp/Server/Controllers/BookingContoller.cs
@@ -0,0 +1,37 @@
+﻿using BlazorChallengeApp.Server.CQRS.Commands.Booking;
+using BlazorChallengeApp.Shared;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorChallengeApp.Server.Controllers
+{
+
+    [Route("[controller]")]
+    public class BookingContoller: ControllerBase
+    {
+        IMediator mediator;
+        public BookingContoller(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> create(Booking booking)
+        {
+            try
+            {
+
+                //Save Booking
+                var response = await mediator.Send(new CreateBooking.Command(booking));
+
+                return Ok(response.Saved);
+            }
+            catch(Exception e)
+            {
+
+                return NotFound("Something Happened.");
+            }
+        }
+
+    }
+}
